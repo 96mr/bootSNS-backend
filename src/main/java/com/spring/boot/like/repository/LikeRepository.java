a1c924e85751c4f0c895998fe1d9e5da4aaa149a
96mr <exmerang@gmail.com>
2024-12-03 18:06:58 +0900

    좋아요 기능 수정

--- src/main/java/com/spring/boot/like/repository/LikeRepository.java
+++ src/main/java/com/spring/boot/like/repository/LikeRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.boot.entity.Board;
-import com.spring.boot.entity.Like;
+import com.spring.boot.entity.BoardLike;
 import com.spring.boot.entity.LikeId;
 
-public interface LikeRepository extends JpaRepository<Like, LikeId> {
-	public List<Like> findByBno(Board bno);
+public interface LikeRepository extends JpaRepository<BoardLike, LikeId> {
+	public List<BoardLike> findByBno(Board bno);
+	public boolean existsByBnoAndLikeId(long bno, long likeId);
 }
